f3a6c2735bd723ab66721e2a3913dc4603f9737d
dawnlunacy <rudd.lacy@gmail.com>
2019-11-01 06:42:11 -0600

    Add logo

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from '../../images/moon-icon.png'
+import logo from '../../images/moon-icon-2.png'
+import './Header.css';
 
 export const Header = () => {
   return (
-    <header>
+    <header className="main-header">
       <h1> Space Insights </h1>
       <img src={logo} alt="A grey asteroid logo" className="logo"/>
     </header>

78e7693175f276d9c14e02de5c2454cd40383a07
dawnlunacy <rudd.lacy@gmail.com>
2019-11-03 13:35:32 -0700

    Rename Neo to AsteroidCardContainer to stop double variable name conflicts, add base logic for rendering AstroidCard

--- src/containers/AsteroidCard/AsteroidCard.js
+++ src/containers/AsteroidCard/AsteroidCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './AsteroidCard.css';
+import { connect } from 'react-redux';
 
-export const AsteroidCard = () => {
-
+export const AsteroidCard = ({currentNeoDate, neos}) => {
+ 
+  
   return (
-    <p> Testing Our Communication</p>
+    <section>
+      <p> Testing Our Communication</p>
+    </section>
+    
   )
 }
+
+export const mapStateToProps = state => ({
+  neos: state.neos,
+  currentNeoDate: state.currentNeoDate
+});
+
+export default connect(mapStateToProps, null)(AsteroidCard)
--- src/containers/AsteroidContainer/AsteroidContainer.js
+++ src/containers/AsteroidContainer/AsteroidContainer.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import '../AsteroidContainer/AsteroidContainer.css';
-import Neos from '../Neos/Neos';
+import AsteroidCard from '../AsteroidCard/AsteroidCard';
+import AsteroidDateCard from '../AstroidDateCard/AsteroidDateCard';
 
-export const AsteroidContainer = ({ image, neos, displayDateSelectedNeos}) => {
- 
+export const AsteroidContainer = ({ image, neos, displayDateSelectedNeos,currentNeoDate}) => {
+
+  if (currentNeoDate !== '') {
+    var asteroidCardsToDisply = neos[currentNeoDate].map(currentNeo => {
+      return <AsteroidCard 
+        key={currentNeo.id}
+        id={currentNeo.id}
+        name={currentNeo.name}
+        nasaUrl={currentNeo.nasaUrl}
+        isPotentiallyHazardous={false}
+        estimatedDiameterMin={currentNeo.estimatedDiameter.estimated_diameter_min}
+        estimatedDiameterMax={currentNeo.estimatedDiameter.estimate_diameter_max}
+        closeApproachDate={currentNeo.closeApproachData.closeApproachDate}
+        relativeVelocity={currentNeo.closeApproachData.relativeVelocity}
+        missEarthDistance={currentNeo.closeApproachData.missEarthDistance}/>
+    })
+  }
     const dateKeys = Object.keys(neos);
     var neoInfoToDisplay = dateKeys.map(currentDate => {
     const date = currentDate;
     const totalNeosOnDate = neos[currentDate].length;
-    return <Neos 
+    return <AsteroidDateCard 
       key={date}
       date={date}
       totalNeosOnDate={totalNeosOnDate}
@@ -27,13 +43,17 @@ export const AsteroidContainer = ({ image, neos, displayDateSelectedNeos}) => {
         <article className="neo-weekday"> 
         {neoInfoToDisplay} 
         </article>
+        <div>
+          {asteroidCardsToDisply}
+        </div>
       </section>
     </main>
   )
 }
 
 export const mapStateToProps = state => ({
-  neos: state.neos
+  neos: state.neos,
+  currentNeoDate: state.currentNeoDate
 });
 
 

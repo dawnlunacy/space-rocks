7d1763bd66c36f3981dad89c74c0c4e974d18f5a
dawnlunacy <rudd.lacy@gmail.com>
2019-11-03 13:53:10 -0700

    Add info to AsteroidCards

--- src/containers/AsteroidCard/AsteroidCard.js
+++ src/containers/AsteroidCard/AsteroidCard.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './AsteroidCard.css';
 import { connect } from 'react-redux';
 
-export const AsteroidCard = ({currentNeoDate, neos}) => {
+export const AsteroidCard = ({id, name, nasaUrl, isPotentiallyHazardous, estimatedDiameterMin, estimatedDiameterMax, closeApproachDate, relativeVelocity, missEarthDistance}) => {
  
   
   return (
     <section>
-      <p> Testing Our Communication</p>
+      <p> ID: {id} </p>
+      <p> Name: {name} </p>
+      <p> Close Approach Date: {closeApproachDate} </p> 
+      <p> Is Potentially Hazardous: {isPotentiallyHazardous} </p>
+      <p> Estimated Diameter Min: {estimatedDiameterMin} Feet </p>
+      <p> Estimated Diameter Max: {estimatedDiameterMax} Feet </p>
+      <p> Relative Velocity: {relativeVelocity} Miles Per Hour</p>
+      <p> Will Miss Earth by: {missEarthDistance} Miles </p>
+      <p> Link to Nasa: {nasaUrl} </p>
     </section>
     
   )

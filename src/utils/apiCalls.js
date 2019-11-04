a9d13d54a3f5d02329a2ab1919e99a59522697ea
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 10:15:40 -0700

    Add in first test - not passing

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,5 +1,5 @@
 export const fetchAPOD = async() => {
-  const imageUrl = 'https://api.nasa.gov/planetary/apod?api_key=m98g3WmabopZXIZRCQ0HdHYrEwuHimuH8b8JjicA'
+  const imageUrl = 'https://api.nasa.gov/planetary/apod?api_key=m98g3WmabopZXIZRCQ0HdHYrEwuHimuH8b8JjicA';
   const response = await fetch(imageUrl)
   if (response.ok) {
     const data = await response.json()
--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -1,7 +1,22 @@
 import { fetchAPOD, fetchNeo } from './apiCalls';
+import { mockNeoDataUnfiltered } from './mockNeoDataUnfiltered';
 
 describe('apiCalls', () => {
   describe('fetchAPOD', () => {
-    
-  })
-})
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockNeoDataUnfiltered)
+        });
+      });
+    });
+
+    it('should fetch with the correct url', () => {
+      const mockUrl = 'https://api.nasa.gov/planetary/apod?api_key=m98g3WmabopZXIZRCQ0HdHYrEwuHimuH8b8JjicA';
+      fetchAPOD();
+      
+      expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+    })
+  });
+});

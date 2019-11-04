6f91f7d74994406a37618f8b0a5e06efbcf980ad
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 01:46:18 -0700

    Add currentNeoDate reducer test

---
+++ src/reducers/currentNeoDate.test.js
@@ -0,0 +1,26 @@
+import { currentNeoDate } from './currentNeoDate';
+
+describe('currentNeoDate', () => {
+  it('should return the initial state', () => {
+    const expected = '';
+
+    const result = currentNeoDate(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return state with the current NEO date being viewed', () => {
+    const mockDate = "2019-11-07"
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: 'SET_CURRENT_NEO_DATE',
+      date: mockDate
+    }
+
+    const newState = mockDate;
+    const result = currentNeoDate(state, action);
+
+    expect(result).toEqual(newState);
+  });
+});

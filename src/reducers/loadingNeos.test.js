073095f0c4dc631f72413fbe9c1a28fcaf78ea14
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 01:51:15 -0700

    Add test for loadingNeos reducer

---
+++ src/reducers/loadingNeos.test.js
@@ -0,0 +1,26 @@
+import { loadingNeos } from './loadingNeos';
+
+describe('loadingNeos', () => {
+  it('should return the initial state', () => {
+    const expected = true;
+
+    const result = loadingNeos(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return state with the updated loading status', () => {
+    const mockBool = false;
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: 'UPDATE_LOADING',
+      bool: mockBool
+    }
+
+    const newState = mockBool;
+    const result = loadingNeos(state, action);
+
+    expect(result).toEqual(newState);
+  });
+});

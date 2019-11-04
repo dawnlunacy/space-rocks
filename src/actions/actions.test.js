8bdfec5bcbe7c5fe65296b66a7c2c8a81b48f741
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 01:16:35 -0700

    Add test for setTotalNeos

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -14,5 +14,139 @@ describe('actions', () => {
     })
   });
 
-  
+  describe('setTotalNeos', () => {
+    it('should have a type of SET_TOTAL_NEOS' , () => {
+      const totalNeos = 10;
+      const result = actions.setTotalNeos(totalNeos);
+
+      const expectedAction = {
+        type: 'SET_TOTAL_NEOS',
+        totalNeos
+      }
+    
+    expect(result).toEqual(expectedAction)
+    });
+  });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+  // describe('setTotalNeos', () => {
+  //   it('should have a type of ' , () => {
+
+  //     const result = actions.
+
+  //     const expectedAction = {
+  //       type: ,
+
+  //     }
+    
+  //   expect(result).toEqual(expectedAction)
+  //   });
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });

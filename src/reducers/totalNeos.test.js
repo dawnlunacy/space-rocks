1873d26448ec60c1f5d42e2e9506439b76aaa02e
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 02:07:50 -0700

    Add test for totalNeos reducer

---
+++ src/reducers/totalNeos.test.js
@@ -0,0 +1,26 @@
+import { totalNeos } from './totalNeos';
+
+describe('totalNeos', () => {
+  it('should return the initial state', () => {
+    const expected = 0;
+
+    const result = totalNeos(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return state with the current total of NEOs found from query', () => {
+    const mockTotal = "2019-11-07"
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: 'SET_TOTAL_NEOS',
+      totalNeos: mockTotal
+    }
+
+    const newState = mockTotal;
+    const result = totalNeos(state, action);
+
+    expect(result).toEqual(newState);
+  });
+});

3cc2c2058d9af4706ee59988d9cced17b756e623
dawnlunacy <rudd.lacy@gmail.com>
2019-11-04 02:04:23 -0700

    Add test for startDate reducer

---
+++ src/reducers/startDate.test.js
@@ -0,0 +1,26 @@
+import { startDate } from './startDate';
+
+describe('startDate', () => {
+  it('should return the initial state', () => {
+    const expected = '';
+
+    const result = startDate(undefined, {});
+
+    expect(result).toEqual(expected);
+  });
+
+  it('should return state with the current start date of NEOs', () => {
+    const mockDate = "2019-11-07"
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: 'SET_START_DATE',
+      date: mockDate
+    }
+
+    const newState = mockDate;
+    const result = startDate(state, action);
+
+    expect(result).toEqual(newState);
+  });
+});
